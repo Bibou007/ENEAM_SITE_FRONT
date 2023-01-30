f76239a8f89382c1836d98892838f5709f36c5f8
Bibou007 <moutalabihabib2002@gmail.com>
2023-01-30 18:49:35 +0100

    Maintenant le hero est slideable ainsi que la section des directeurs

---
+++ js/app.js
@@ -0,0 +1,76 @@
+// $(document).ready(function(){
+//     // =======hero-slider=========
+//     $('#hero-slider').owlCarousel({
+//         loop:true,
+//         margin:0,
+//         nav:true,
+//         items: 1,
+//         dots: false,
+//         smartSpeed: 1000,
+//         navText: ["PREV", "NEXT"],
+//         responsive:{
+//             0:{
+                
+//             },
+//             600:{
+                
+//             },
+//             1000:{
+                
+//             }
+//         }
+//     })
+//   });
+  
+  $(document).ready(function(){
+    // =======hero-slider=========
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        navText: ["PREV", "NEXT"],
+        responsive:{
+            0:{
+                nav : false
+            },
+            768:{
+                nav : true
+            },
+            1000:{
+                
+            }
+        }
+    });
+
+    $('#directeurs-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:true,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        responsive:{
+            0:{
+                items: 2,
+                nav: false,
+            },
+            768:{
+                items: 2,
+            },
+            1140:{
+                items: 3,
+            }
+        }
+
+    })
+
+  });
+  
